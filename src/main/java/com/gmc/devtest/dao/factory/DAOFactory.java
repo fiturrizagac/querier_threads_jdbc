da7bda19d240d15bc88f84e4051ad0901d1a0c53
Franco Iturrizaga <fiturrizagac@gmail.com>
2016-02-14 00:48:51 -0500

    -Added DAOFctory pattern

---
+++ src/main/java/com/gmc/devtest/dao/factory/DAOFactory.java
@@ -0,0 +1,12 @@
+package com.gmc.devtest.dao.factory;
+
+import com.gmc.devtest.dao.SampleDAO;
+
+/**
+ * Created by BRIGETH on 14/02/2016.
+ */
+public interface DAOFactory {
+
+    SampleDAO getSampleDAO();
+
+}
